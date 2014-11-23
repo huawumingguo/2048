2ff7fc889a83dd3d6d825b4fc976c8ff7979af44
huawumingguo <dguoming@gmail.com>
2014-11-24 00:23:51 +0800

    表达为中文了, ,得分还没改 ,两个字显示的时候,颜色之类的 ,在宇宙的时候成功处理还没有

--- js/application.js
+++ js/application.js
@@ -1,3 +1,12 @@
+var nextVal1 = function(val){
+	var arr = ['这','宜','江','中','亚','世'];
+	var pindex = arr.indexOf(val);
+	if(pindex!=-1){
+		return arr[pindex+1];
+	}
+};
+
+
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
